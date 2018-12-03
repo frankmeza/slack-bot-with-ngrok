8fa392f7e15e8b855fb837ca35dae63ff94e444b
Cory West-Forbes <cory.westforbes@olioapps.com>
2018-12-03 10:30:12 -0800

    read firebase

--- command_handlers/til.js
+++ command_handlers/til.js
@@ -6,19 +6,54 @@ const handleTodayILearned = (req, res) => {
     const { body } = req
 
     const database = firebase.database()
-    const now = new Date()
-    const date = `${now.getMonth() + 1}_${now.getDate()}_${now.getFullYear()}`
 
-    database
-        .ref("users/" + body.user_id + "/til/" + date)
-        .push()
-        .update({ text: body.text, ts: Date.now() })
+    const now = new Date()
+    const month = now.getMonth() + 1
+    const dateMonth = now.getDate()
+    const year = now.getFullYear()
+
+    const date = `${month}_${dateMonth}_${year}`
+
+    // database
+    //     .ref(`users/${body.user_id}/til/${date}`)
+    //     .push()
+    //     .update({ text: body.text, ts: Date.now() })
+
+    // // query then return number of things learned today
+    // // const numberofTIL = tilRef.once("value", function(snapshot) {
+    // //     // return Object.keys(snapshot).length
+    // //     return snapshot
+    // // })
+
+    const hello = {
+    }
+
+    const tilRef = database.ref(`users/${body.user_id}/til/11_15_2018`)
+
+
+    const gotData = (data) => {
+        const entries = data.val()
+        console.log(entries)
+        const keys = Object.keys(entries)
+        console.log(keys)
+
+        hello.yo = keys
+
+    }
+
+    const errData = (err) => {
+        console.log("Error!")
+        console.log(err)
+    }
+
+
+    tilRef.on("value", gotData, errData)
 
     res.json({
-        text: "Awesome! Your TIL has been recorded :white_check_mark:",
+        text: "Awesome! Your TIL has been recorded",
         attachments: [
             {
-                text: body.text,
+                text: `Today you learned ${hello.yo.length} things`,
             },
         ],
     })
